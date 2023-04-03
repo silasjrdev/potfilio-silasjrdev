1939a22ea6625a4a3ea9a397e1106b13937a4c31
Silas Junior <silas.mcoach@gmail.com>
2023-04-02 23:03:41 -0300

    feat: add data user

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Silas Marques",
+  githubUser: "silasjrdev",
+  linkedinUser: "silasmjunior",
+  whatsappNumber: "+5521982157940",
 };
